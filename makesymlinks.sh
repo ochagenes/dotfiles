79c0bce34b1cdb54da0108954f4a348ced009270
Ole-Christian S. Hagenes <ole@hagenes.net>
2013-03-24 13:05:26 +0100

    Added ssh to makesymlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gvimrc irssi zshrc mplayer Xdefaults xmodmap"    # list of files/folders to symlink in homedir
+files="ssh/config bashrc vimrc gvimrc irssi zshrc mplayer Xdefaults xmodmap"    # list of files/folders to symlink in homedir
 
 ##########
 
